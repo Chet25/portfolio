710c2405f86434699ac7b2466f23bd765e3cb9a2
Chet25 <chetan25december@gmail.com>
2026-01-06 08:44:27 +0530

    pathing fixes for new home to be dashboard

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,10 +2,15 @@
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Welcome {{ Str::ucfirst(auth()->user()->name) }}</span>
+    @auth
+        <span class="mb-0.5 truncate leading-tight font-semibold">Welcome {{ Str::ucfirst(auth()->user()->name) }}</span>
 
-    <span
-        class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10 dark:bg-blue-400/10 dark:text-blue-400 dark:ring-blue-400/30">
-        {{auth()->user()->getRoleNames()->first()}}
-    </span>
+        <span
+            class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10 dark:bg-blue-400/10 dark:text-blue-400 dark:ring-blue-400/30">
+            {{auth()->user()->getRoleNames()->first()}}
+        </span>
+    @else
+        <span class="mb-0.5 truncate leading-tight font-semibold">{{ config('app.name', 'Chetan') }}</span>
+        <span class="text-xs text-zinc-500">Portfolio</span>
+    @endauth
 </div>
